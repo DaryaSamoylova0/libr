0b1c565b4060f69d538364b72eceb8c2cf698c94
Дарья Самойлова <dasha_samoylova_02@mail.ru>
2023-04-24 03:37:05 +0500

    подключение к бд

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const  express = require('express')
+const sequelize = require('./db')
 
 const PORT = process.env.PORT || 5000
 
@@ -7,4 +8,14 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+start()
